5e362bbe41520176a27da70705eefaebf3e320ac
Hui Ru Tong <huirutong06@gmail.com>
2026-09-01 19:58:14 +0800

    added another run to see if the rewards give consistent results

--- main.py
+++ main.py
@@ -21,12 +21,13 @@ max_steps = 25
 
 actor = ActorCritic()
 agent = PPO(num_epochs, num_transitions, minibatch_size, lr, step_size, lr_gamma, gamma, lmbda, clip_epsilon, entropy_coef)
-env = Charge_Env(max_charge, max_steps, split_improvement_rwd)
+env = Charge_Env(max_charge, max_steps, tot_improvement_rwd)
 found_charges = []
 
-log_file = open("./found_charges_2.txt", "w")
+log_file = open("./found_charges.txt", "w")
 
 for i in range(num_iterations):
+
     print(f"Currently on iteration {i+1} of {num_iterations}")
 
     states = []
